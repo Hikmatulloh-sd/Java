84a520926e5eff5b6fb218500d8017d546955d70
Hikmatulloh-sd <hikmatulloh.sd@gmail.com>
2025-01-24 11:39:00 +0600

    Variables & Primitive data types

---
+++ variables/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args){
+        // Int 32 
+        int myInt;
+        myInt = 557;
+        // Int 16
+        short myShort = 3266;
+        // Int 62
+        long myLong = 26246446;
+        System.out.println(myInt);
+
+        // 34 bit 
+        float myFloat = 235.33f;
+        // 64 bit 
+        double myDouble = 2345.456;
+
+        char a = 'a';
+
+        boolean isFree = true;
+
+        // int 8 
+        byte myByte = 100; // -128 127
+    }
+}
