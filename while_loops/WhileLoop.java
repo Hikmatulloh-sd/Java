7a8060d1fd7427331a81222eb18fa064cb842c04
Hikmatulloh-sd <hikmatulloh.sd@gmail.com>
2025-01-24 12:29:27 +0600

    While Loop

---
+++ while_loops/WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+    public static void main(String[] args){
+        // > < >= <= == !==
+        // boolean bool = 5 < 6;
+        // System.out.println(bool);
+
+        int value = 0;
+
+        while(value < 10){
+            value = value += 1;
+            System.out.println(value);
+        }
+    }
+}
